4bfdc48f9c5508f6ff4c40bd029611c90a51546a
TuNombre <sebassando557@gmail.com>
2025-06-10 18:10:29 -0500

    Primer commit: proyecto chat San Valentín

--- server.js
+++ server.js
@@ -1,26 +1,32 @@
-// server.js
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const path = require('path');
 
-const PORT = process.env.PORT || 3000;
+// Sirve archivos estáticos desde /public
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.static('public'));
-
-io.on('connection', (socket) => {
-    console.log('Un usuario se ha conectado');
-
-    socket.on('chat message', (data) => {
-        // Broadcast a todos los usuarios
-        io.emit('chat message', data);
-    });
-
-    socket.on('disconnect', () => {
-        console.log('Un usuario se ha desconectado');
-    });
+// Ruta raíz
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+// Socket.io
+io.on('connection', (socket) => {
+  console.log('Usuario conectado');
+
+  socket.on('chat message', (data) => {
+    io.emit('chat message', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Usuario desconectado');
+  });
+});
+
+// Puerto
+const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
