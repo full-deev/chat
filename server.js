ed7def47fa9085ec8e888d3e3a6345f61933a032
Camilo <74275386+full-deev@users.noreply.github.com>
2025-06-10 18:30:21 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -1,32 +1,26 @@
+// server.js
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
-const path = require('path');
 
-// Sirve archivos estáticos desde /public
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Ruta raíz
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-// Socket.io
-io.on('connection', (socket) => {
-  console.log('Usuario conectado');
-
-  socket.on('chat message', (data) => {
-    io.emit('chat message', data);
-  });
-
-  socket.on('disconnect', () => {
-    console.log('Usuario desconectado');
-  });
-});
-
-// Puerto
 const PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+    console.log('Un usuario se ha conectado');
+
+    socket.on('chat message', (data) => {
+        // Broadcast a todos los usuarios
+        io.emit('chat message', data);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Un usuario se ha desconectado');
+    });
+});
+
 http.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
